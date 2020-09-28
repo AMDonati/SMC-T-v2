2b184e783a43997043c8929ee7be4a5a2aed5b51
AMDonati <alice.martindonati@gmail.com>
2020-09-28 17:25:42 +0200

    sh script preprocess datasets

---
+++ preprocess_datasets.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+python src/preprocessing/create_synthetic_dataset.py -model 1 -num_samples 1000 -seq_len 24 -num_features 1
+python src/preprocessing/create_synthetic_dataset.py -model 2 -alpha 0.9 -variance 0.3 -num_samples 1000 -seq_len 24
+python src/preprocessing/preprocess_covid_data.py
+python src/preprocessing/preprocess_weather_dataset.py
+python src/preprocessing/preprocess_air_quality.py
