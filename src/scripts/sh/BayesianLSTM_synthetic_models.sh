a5314ee4d9545f7f3bf4b41560999fd5ae02881e
AMDonati <alice.martindonati@gmail.com>
2020-09-29 15:43:02 +0200

    sh script

--- src/scripts/sh/BayesianLSTM_synthetic_models.sh
+++ src/scripts/sh/BayesianLSTM_synthetic_models.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 python src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "bayesian_lstm" -rnn_units 32 -bs 32 -ep 50 -output_path "output/exp_synthetic_model_1" -mc_samples 1000 -cv 1
-python src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "bayesian_lstm" -rnn_units 64 -bs 32 -ep 50 -output_path "output/exp_synthetic_model_1" -inference 1 -mc_samples 1000 -cv 1
-python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2" -algo "bayesian_lstm" -rnn_units 32 -bs 32 -ep 50 -output_path "output/exp_synthetic_model_2" -inference 1 -mc_samples 1000 -cv 1
-python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2" -algo "bayesian_lstm" -rnn_units 64 -bs 32 -ep 50 -output_path "output/exp_synthetic_model_2" -inference 1 -mc_samples 1000 -cv 1
+python src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "bayesian_lstm" -rnn_units 64 -bs 32 -ep 50 -output_path "output/exp_synthetic_model_1" -mc_samples 1000 -cv 1
+python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2" -algo "bayesian_lstm" -rnn_units 32 -bs 32 -ep 50 -output_path "output/exp_synthetic_model_2" -mc_samples 1000 -cv 1
+python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2" -algo "bayesian_lstm" -rnn_units 64 -bs 32 -ep 50 -output_path "output/exp_synthetic_model_2" -mc_samples 1000 -cv 1
