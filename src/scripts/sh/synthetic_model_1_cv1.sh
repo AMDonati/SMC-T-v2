7f89f4b28562a2ee2ac38091cf255694c46a08da
AMDonati <alice.martindonati@gmail.com>
2020-10-01 21:20:55 +0200

    sh script

--- src/scripts/sh/synthetic_model_1_cv1.sh
+++ src/scripts/sh/synthetic_model_1_cv1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 16 -dff 16 -bs 32 -ep 50 -smc False -output_path "output/exp_synthetic_model_1" -cv 1 -alpha 0.8
-python src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 16 -dff 16 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/exp_synthetic_model_1" -cv 1 -alpha 0.8
+python src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -smc False -output_path "output/exp_synthetic_model_1" -cv 1 -alpha 0.8
+python src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/exp_synthetic_model_1" -cv 1 -alpha 0.8
 python src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 16 -dff 16 -bs 32 -ep 50 -particles 30 -smc True -output_path "output/exp_synthetic_model_1" -cv 1 -alpha 0.8
--- src/scripts/sh/synthetic_model_2_cv1.sh
+++ src/scripts/sh/synthetic_model_2_cv1.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2" -algo "smc_t" -d_model 8 -dff 8 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/exp_synthetic_model_2" -cv 1
-python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2" -algo "smc_t" -d_model 8 -dff 8 -bs 32 -ep 50 -particles 30 -smc True -output_path "output/exp_synthetic_model_2" -cv 1
-python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2" -algo "smc_t" -d_model 16 -dff 16 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/exp_synthetic_model_2" -cv 1
-python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2" -algo "smc_t" -d_model 16 -dff 16 -bs 32 -ep 50 -particles 30 -smc True -output_path "output/exp_synthetic_model_2" -cv 1
+python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -smc False -output_path "output/exp_synthetic_model_2" -cv 1
+python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/exp_synthetic_model_2" -cv 1
+python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 30 -smc True -output_path "output/exp_synthetic_model_2" -cv 1
