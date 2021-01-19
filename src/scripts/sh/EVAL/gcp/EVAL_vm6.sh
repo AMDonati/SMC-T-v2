f2104770bf88d9a905b08d54d3a420400125788a
AMDonati <alice.martindonati@gmail.com>
2021-01-19 10:41:15 +0100

    sh eval

---
+++ src/scripts/sh/EVAL/gcp/EVAL_vm6.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+python src/scripts/run.py -dataset "stock" -data_path "data/stock" -algo "baseline_t" -d_model 32 -dff 32 -bs 64 -ep 0 -output_path "output/exp_stock" -p_drop 0.1 -mc_samples 1000 -multistep 1 -past_len 20 -save_path "output/exp_stock/baseline_t_d32_p0.1/1"
+python src/scripts/run.py -dataset "stock" -data_path "data/stock" -algo "baseline_t" -d_model 32 -dff 32 -bs 64 -ep 0 -output_path "output/exp_stock" -p_drop 0.5 -mc_samples 1000 -multistep 1 -past_len 20 -save_path "output/exp_stock/baseline_t_d32_p0.5/1"
+python src/scripts/run.py -dataset "stock" -data_path "data/stock" -algo "lstm" -rnn_units 32 -bs 64 -ep 0 -output_path "output/exp_stock" -p_drop 0.1 -mc_samples 1000 -multistep 1 -past_len 20 -save_path "output/exp_stock/lstm_d32_p0.1/1"
+python src/scripts/run.py -dataset "stock" -data_path "data/stock" -algo "lstm" -rnn_units 32 -bs 64 -ep 0 -output_path "output/exp_stock" -p_drop 0.5 -mc_samples 1000 -multistep 1 -past_len 20 -save_path "output/exp_stock/lstm_d32_p0.5/1"
+python src/scripts/run.py -dataset "stock" -data_path "data/stock" -algo "bayesian_lstm" -rnn_units 32 -bs 64 -ep 0 -output_path "output/exp_stock" -mc_samples 1000 -particles 3 -multistep 1 -past_len 20 -save_path "output/exp_stock/bayesian_lstm_d32/1"
