bec5a9f625a918dc065d197fe2111c1185b757b4
Alice Martin <alice.martindonati@gmail.com>
2021-01-28 20:42:08 +0100

    sh

--- src/scripts/sh/baselines_weather.sh
+++ src/scripts/sh/baselines_weather.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "baseline_t" -d_model 32 -dff 32 -bs 256 -ep 50 -output_path "output/exp_weather"
-#python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "baseline_t" -d_model 32 -dff 32 -bs 256 -ep 50 -output_path "output/exp_weather" -p_drop 0.1 -mc_samples 1000
-#python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "baseline_t" -d_model 32 -dff 32 -bs 256 -ep 50 -output_path "output/exp_weather" -p_drop 0.2 -mc_samples 1000
-#python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "baseline_t" -d_model 32 -dff 32 -bs 256 -ep 50 -output_path "output/exp_weather" -p_drop 0.5 -mc_samples 1000
-#python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "lstm" -rnn_units 32 -bs 256 -ep 50 -output_path "output/exp_weather"
-python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "lstm" -rnn_units 32 -bs 256 -ep 50 -output_path "output/exp_weather_lstm" -p_drop 0.1 -mc_samples 1000 -multistep 1 -past_len 12
-python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "lstm" -rnn_units 32 -bs 256 -ep 50 -output_path "output/exp_weather_lstm" -p_drop 0.2 -mc_samples 1000 -multistep 1 -past_len 12
-python src/scripts/run.py -dataset "weather" -data_path "data/weather_vm3" -algo "lstm" -rnn_units 32 -bs 256 -ep 50 -output_path "output/exp_weather_lstm" -p_drop 0.5 -mc_samples 1000 -multistep 1 -past_len 12
+python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "baseline_t" -d_model 32 -dff 32 -bs 256 -ep 50 -output_path "output/exp_weather" -max_samples 50000
+python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "baseline_t" -d_model 32 -dff 32 -bs 256 -ep 50 -output_path "output/exp_weather" -p_drop 0.1 -mc_samples 1000 -max_samples 50000
+python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "baseline_t" -d_model 32 -dff 32 -bs 256 -ep 50 -output_path "output/exp_weather" -p_drop 0.2 -mc_samples 1000 -max_samples 50000
+python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "baseline_t" -d_model 32 -dff 32 -bs 256 -ep 50 -output_path "output/exp_weather" -p_drop 0.5 -mc_samples 1000 -max_samples 50000
+python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "lstm" -rnn_units 32 -bs 256 -ep 50 -output_path "output/exp_weather" -max_samples 50000
+python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "lstm" -rnn_units 32 -bs 256 -ep 50 -output_path "output/exp_weather" -p_drop 0.1 -mc_samples 1000 -multistep 1 -past_len 12 -max_samples 50000
+python src/scripts/run.py -dataset "weather" -data_path "data/weather" -algo "lstm" -rnn_units 32 -bs 256 -ep 50 -output_path "output/exp_weather" -p_drop 0.2 -mc_samples 1000 -multistep 1 -past_len 12 -max_samples 50000
+python src/scripts/run.py -dataset "weather" -data_path "data/weather_vm3" -algo "lstm" -rnn_units 32 -bs 256 -ep 50 -output_path "output/exp_weather" -p_drop 0.5 -mc_samples 1000 -multistep 1 -past_len 12 -max_samples 50000
