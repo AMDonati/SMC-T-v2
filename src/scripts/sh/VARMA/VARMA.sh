993d9b24f9e284163e0043434632d3eb7172c65d
Alice Martin <alice.martindonati@gmail.com>
2021-02-02 12:10:57 +0100

    sh

--- src/scripts/sh/VARMA/VARMA.sh
+++ src/scripts/sh/VARMA/VARMA.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo "-------- air quality ---------------------------------------------"
 #python src/scripts/run.py -dataset "air_quality" -data_path "data/air_quality" -algo "varma" -p_model 1 -q 0 -ep 500 -output_path "output/exp_air_quality" -multistep 1 -past_len 6
-python src/scripts/run.py -dataset "air_quality" -data_path "data/air_quality" -algo "varma" -p_model 1 -q 0 -ep 1000 -output_path "output/exp_air_quality" -multistep 1 -past_len 6
-python src/scripts/run.py -dataset "air_quality" -data_path "data/air_quality" -algo "varma" -p_model 3 -q 0 -ep 1000 -output_path "output/exp_air_quality" -multistep 1 -past_len 6
-python src/scripts/run.py -dataset "air_quality" -data_path "data/air_quality" -algo "varma" -p_model 5 -q 0 -ep 1000 -output_path "output/exp_air_quality" -multistep 1 -past_len 6
+#python src/scripts/run.py -dataset "air_quality" -data_path "data/air_quality" -algo "varma" -p_model 1 -q 0 -ep 1000 -output_path "output/exp_air_quality" -multistep 1 -past_len 6
+#python src/scripts/run.py -dataset "air_quality" -data_path "data/air_quality" -algo "varma" -p_model 3 -q 0 -ep 1000 -output_path "output/exp_air_quality" -multistep 1 -past_len 6
+#python src/scripts/run.py -dataset "air_quality" -data_path "data/air_quality" -algo "varma" -p_model 5 -q 0 -ep 1000 -output_path "output/exp_air_quality" -multistep 1 -past_len 6
 echo "-------- stock ---------------------------------------------"
 python src/scripts/run.py -dataset "stock" -data_path "data/stock" -algo "varma" -p_model 1 -q 0 -ep 1000 -output_path "output/exp_stock" -multistep 1 -past_len 20
 echo "-------- energy ---------------------------------------------"
