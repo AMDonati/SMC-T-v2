ad45f51b2dfbafcc1461a677c849364e069ceec7
Alice Martin <alice.martindonati@gmail.com>
2021-06-28 10:09:13 +0200

    sh

--- src/scripts/sh/synthetic_models/synthetic_models_no_noise/synthetic_model_2_cv1.sh
+++ src/scripts/sh/synthetic_models/synthetic_models_no_noise/synthetic_model_2_cv1.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2_1e-05var" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -smc False -output_path "output/exp_synthetic_model_2" -cv 1
-python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2_1e-05var" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/exp_synthetic_model_2" -cv 1
-python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2_1e-05var" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 30 -smc True -output_path "output/exp_synthetic_model_2" -cv 1
+python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2_1e-05var" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -smc False -output_path "output/exp_synthetic_model_2" -cv 1 -alpha 0.9
+python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2_1e-05var" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/exp_synthetic_model_2" -cv 1 -alpha 0.9
+python src/scripts/run.py -dataset "synthetic" -dataset_model 2 -data_path "data/synthetic_model_2_1e-05var" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 30 -smc True -output_path "output/exp_synthetic_model_2" -cv 1 -alpha 0.9
