c5009e7ed39ffa61eed260d4d8967b8a2c2f414c
AMDonati <alice.martindonati@gmail.com>
2021-10-05 21:03:50 +0200

    save modelss script

--- src/scripts/save_datasets_models.py
+++ src/scripts/save_datasets_models.py
@@ -13,16 +13,16 @@ tokenizer.save_pretrained("cache/gpt2")
 print("done loading hugging face models")
 
 
-for path in ["data/sst/train", "data/sst/val", "data/sst/test", "data/sst/all_data"]:
-    if not os.path.isdir(path):
-        os.makedirs(path)
-
-dataset = load_dataset('sst')
-train_set, val_set, test_set = dataset["train"], dataset["validation"], dataset["test"]
-train_set.save_to_disk("cache/sst/train")
-val_set.save_to_disk("cache/sst/val")
-test_set.save_to_disk("cache/sst/test")
-all_dataset = load_dataset("sst", split='train+validation+test')
-all_dataset.save_to_disk("cache/sst/all_data")
-
-print("done loading sst dataset")
+# for path in ["data/sst/train", "data/sst/val", "data/sst/test", "data/sst/all_data"]:
+#     if not os.path.isdir(path):
+#         os.makedirs(path)
+#
+# dataset = load_dataset('sst')
+# train_set, val_set, test_set = dataset["train"], dataset["validation"], dataset["test"]
+# train_set.save_to_disk("cache/sst/train")
+# val_set.save_to_disk("cache/sst/val")
+# test_set.save_to_disk("cache/sst/test")
+# all_dataset = load_dataset("sst", split='train+validation+test')
+# all_dataset.save_to_disk("cache/sst/all_data")
+#
+# print("done loading sst dataset")
