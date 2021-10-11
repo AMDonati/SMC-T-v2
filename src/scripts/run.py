c4af16f505ec2c17af58bb6a841e3dee874f9a71
AMDonati <alice.martindonati@gmail.com>
2021-10-11 20:06:33 +0200

    sh

--- src/scripts/run.py
+++ src/scripts/run.py
@@ -61,7 +61,10 @@ def get_parser():
     parser.add_argument("-lr", type=float, default=0.001, help="learning rate")
     # smc params.
     parser.add_argument("-particles", type=int, default=1, help="number of particles")
-    parser.add_argument("-sigmas", type=float, default=0.5, help="values for sigma_k, sigma_q, sigma_v, sigma_z")
+    parser.add_argument("-sigmas", type=float, default=[0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1,
+                                                        0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.20, 0.21,
+                                                        0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.30, 0.31, 0.32, 0.33],
+                                                        help="values for sigma_k, sigma_q, sigma_v, sigma_z")
     parser.add_argument("-noise_dim", type=str, default="uni", help="1D noise or multi-dim noise.")
     parser.add_argument("-smc", type=str2bool, default=False, help="Recurrent Transformer with or without smc algo")
     # output_path params.
