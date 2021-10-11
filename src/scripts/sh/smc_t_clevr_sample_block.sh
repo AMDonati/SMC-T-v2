53c4690748d287963b791ea1b2a086f91d931439
AMDonati <alice.martindonati@gmail.com>
2021-10-11 17:39:47 +0200

    correct bug with negative sigma values in sh

--- src/scripts/sh/smc_t_clevr_sample_block.sh
+++ src/scripts/sh/smc_t_clevr_sample_block.sh
@@ -26,6 +26,6 @@ BS=32
 PARTICLES=10
 EP=20
 
-srun python -u src/scripts/run.py -dataset $DATASET -data_path $DATA_PATH -algo "smc_t" -d_model $D_MODEL -dff $DFF -bs $BS -ep $EP -output_path $OUTPUT_PATH -smc True -particles $PARTICLES -sigmas -0.1  -test_samples 30 -max_seq_len 20 -sampl_freq 1
-srun python -u src/scripts/run.py -dataset $DATASET -data_path $DATA_PATH -algo "smc_t" -d_model $D_MODEL -dff $DFF -bs $BS -ep $EP -output_path $OUTPUT_PATH -smc True -particles $PARTICLES -sigmas -0.1  -test_samples 30 -max_seq_len 20 -sampl_freq 2
-srun python -u src/scripts/run.py -dataset $DATASET -data_path $DATA_PATH -algo "smc_t" -d_model $D_MODEL -dff $DFF -bs $BS -ep $EP -output_path $OUTPUT_PATH -smc True -particles $PARTICLES -sigmas -0.1  -test_samples 30 -max_seq_len 20 -sampl_freq 4
+srun python -u src/scripts/run.py -dataset $DATASET -data_path $DATA_PATH -algo "smc_t" -d_model $D_MODEL -dff $DFF -bs $BS -ep $EP -output_path $OUTPUT_PATH -smc True -particles $PARTICLES -sigmas 0.1  -test_samples 30 -max_seq_len 20 -sampl_freq 1
+srun python -u src/scripts/run.py -dataset $DATASET -data_path $DATA_PATH -algo "smc_t" -d_model $D_MODEL -dff $DFF -bs $BS -ep $EP -output_path $OUTPUT_PATH -smc True -particles $PARTICLES -sigmas 0.1  -test_samples 30 -max_seq_len 20 -sampl_freq 2
+srun python -u src/scripts/run.py -dataset $DATASET -data_path $DATA_PATH -algo "smc_t" -d_model $D_MODEL -dff $DFF -bs $BS -ep $EP -output_path $OUTPUT_PATH -smc True -particles $PARTICLES -sigmas 0.1  -test_samples 30 -max_seq_len 20 -sampl_freq 4
