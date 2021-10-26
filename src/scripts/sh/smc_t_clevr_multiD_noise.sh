5ed94b73a26ab2fb96768bbaa8aae4f643a373fb
AMDonati <alice.martindonati@gmail.com>
2021-10-26 19:10:05 +0200

    sh

--- src/scripts/sh/smc_t_clevr_multiD_noise.sh
+++ src/scripts/sh/smc_t_clevr_multiD_noise.sh
@@ -24,7 +24,7 @@ D_MODEL=32
 DFF=32
 BS=32
 PARTICLES=10
-EP=40
+EP=20
 
 srun python -u src/scripts/run.py -dataset $DATASET -data_path $DATA_PATH -algo "smc_t" -d_model $D_MODEL -dff $DFF -bs $BS -ep $EP -output_path $OUTPUT_PATH -smc True -particles $PARTICLES -sigmas 0.5  -test_samples 30 -max_seq_len 20 -noise_dim "multi"
 #OUTPUT_PATH="output/NLP/CLEVR/multiD_noise/multiple_init"
