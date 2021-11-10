00ecac2d3d90bf768afcd118aa09e8dc0dd26ce9
AMDonati <alice.martindonati@gmail.com>
2021-11-10 09:57:06 +0100

    sh

--- src/scripts/sh/smc_t_clevr_sigma0.5_40ep.sh
+++ src/scripts/sh/smc_t_clevr_sigma0.5_40ep.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=CLEVR-smc-t0.5
+#SBATCH --job-name=40ep-CLEVR-smc-t0.5
 #SBATCH --qos=qos_gpu-t3
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:2
 #SBATCH --array=1-4
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=8g
-#SBATCH --output=slurm_out/sst/CLEVR-smc_t0.5-%j.out
-#SBATCH --error=slurm_out/sst/CLEVR-smc_t0.5-%j.err
+#SBATCH --output=slurm_out/sst/CLEVR-smc_t0.5-40ep-%j.out
+#SBATCH --error=slurm_out/sst/CLEVR-smc_t0.5-40ep-%j.err
 #SBATCH --time=20:00:00
 #SBATCH -A ktz@gpu
 
@@ -25,12 +25,10 @@ D_MODEL=32
 DFF=32
 BS=32
 PARTICLES=10
-EP=20
+EP=40
 
 set -x
 echo "now processing task id:: " ${SLURM_ARRAY_TASK_ID}
 OUT_PATH=output/NLP/CLEVR/NEW_EXP/${SLURM_ARRAY_TASK_ID}
 
 srun python -u src/scripts/run.py -dataset $DATASET -data_path $DATA_PATH -algo "smc_t" -d_model $D_MODEL -dff $DFF -bs $BS -ep $EP -output_path ${OUT_PATH} -smc True -particles $PARTICLES -sigmas 0.5 -max_seq_len 20 -full_model True
-
-#srun python -u src/scripts/run.py -dataset $DATASET -data_path $DATA_PATH -algo "smc_t" -d_model $D_MODEL -dff $DFF -bs $BS -ep 40 -output_path $OUTPUT_PATH -smc True -particles $PARTICLES -sigmas 0.5 -max_seq_len 20 -full_model True
