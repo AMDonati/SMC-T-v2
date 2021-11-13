db30916483aaab03e08079355592188afa784fca
AMDonati <alice.martindonati@gmail.com>
2021-11-13 08:37:34 +0100

    merge metrics

--- src/post-processing/merge_metrics.py
+++ src/post-processing/merge_metrics.py
@@ -8,41 +8,10 @@ def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-path", type=str, required=True,
                         help="data folder containing experiments")
-    parser.add_argument('-columns_to_save', nargs='+', type=str,
-                        default=["oracle_score", "recall_5", "bleu", "meteor", "cider", "ppl_dialog_lm",
-                                 "language_score", "selfbleu", "kurtosis", "peakiness"], help="")
-    parser.add_argument('-bottom_folder', type=int, default=1)
-    parser.add_argument('-top_folder', type=int, default=1)
+    #parser.add_argument('-bottom_folder', type=int, default=1)
+    #parser.add_argument('-top_folder', type=int, default=1)
     parser.add_argument('-precision', type=int, default=4)
-
     return parser
-
-
-def add_to_metrics(df, path_exp):
-    pattern = "test_(test_images|test_text)_(no_trunc|with_trunc)_(sampling_ranking_lm|sampling|greedy)_vilbert_recall_rewards.csv"
-    all = {"no_trunc": {}, "with_trunc": {}}
-    if "oracle_score" not in df.index:
-        means = {trunc: [] for trunc in list(df.columns)}
-        root, _, files = next(os.walk(os.path.join(path_exp, "metrics")))
-        f_patterns = {f: re.findall(pattern, f) for f in files}
-        for f, patterns in f_patterns.items():
-            if "train" not in f and patterns and len(patterns[0]) == 3:
-                try:
-                    mode, trunc, sampl = patterns[0]
-                    id = "_".join([mode, sampl, "oracle_score"])
-                    rew_df = pd.read_csv(os.path.join(root, f), header=None)
-                    mean_rew = rew_df[0].mean()
-                    all[trunc][id] = mean_rew
-
-                except pd.errors.EmptyDataError:
-                    print("empty file {}".format(os.path.join(root, f)))
-                    continue
-        oracle_score = pd.DataFrame.from_dict(all)
-        oracle_score.to_csv(os.path.join(path_exp, "stats", "oracle_score.csv"), )
-        oracle_score_serie = oracle_score.mean()
-        oracle_score_serie.name = "oracle_score"
-        df = df.append(oracle_score_serie)
-    return df
 
 def str_to_float_in_csv(df, index="train_loss", sep=","):
     values = df.loc[index].values[-1]
@@ -50,13 +19,27 @@ def str_to_float_in_csv(df, index="train_loss", sep=","):
     values = [e.replace('[', '') for e in values]
     values = [e.replace(']', '') for e in values]
     values = [e.replace('\n', '') for e in values]
-    values = [float(e) for e in values]
-    return values[-1]
+    if len(values) == 1 and '' in values:
+        return None
+    else:
+        values = [float(e) for e in values]
+        return values[-1]
 
-def merge_one_experiment(path="output/NLP/CLEVR/NEW_EXP__OLD"):
+def check_train_mse_metric(df):
+    values = df.loc["train_mse_metric"].values[-1]
+    values = values.split(',')
+    values = [e.replace('[', '') for e in values]
+    values = [e.replace(']', '') for e in values]
+    values = [e.replace('\n', '') for e in values]
+    if len(values) == 1 and '' in values:
+        return None
+    else:
+        values = [float(e) for e in values]
+        return values[-1]
+
+def merge_one_experiment(path="output/temp", precision=4):
     dirs = [f.path for f in os.scandir(path) if f.is_dir()]
-    merge_metrics_mean = pd.DataFrame()
-
+    merge_metrics = pd.DataFrame()
     for dir_conf in dirs:
         dirs = [f.path for f in os.scandir(dir_conf) if f.is_dir()]
         stats_all_runs_df = pd.DataFrame()
@@ -66,22 +49,30 @@ def merge_one_experiment(path="output/NLP/CLEVR/NEW_EXP__OLD"):
             if os.path.exists(losses_path):
                 losses_exp = pd.read_csv(losses_path, index_col=0, header=None)
                 losses_values = []
-                for index in ["train_loss", "val_loss", "train_mse_metric", "val_mse_metric"]:
-                    sep = "," if index in ["train_loss", "val_loss", "train_mse_metric", "val_mse_metric"] else " "
-                    losses_values.append(str_to_float_in_csv(losses_exp, index, sep=sep))
-                all_metrics_df = pd.Series(losses_values, index=["train_loss", "val_loss", "train_mse_metric", "val_mse_metric"])
-                all_metrics_df.loc["train_ppl"] = math.exp(all_metrics_df["train_mse_metric"])
-                all_metrics_df.loc["val_ppl"] = math.exp(all_metrics_df["val_mse_metric"])
+                value_train_mse = check_train_mse_metric(losses_exp)
+                indexes = ["train_loss", "val_loss"] if value_train_mse is None else ["train_mse_metric", "val_mse_metric"]
+                for index in indexes:
+                    losses_values.append(str_to_float_in_csv(losses_exp, index, sep=','))
+                all_metrics_df = pd.Series(losses_values, index=["train_ce", "val_ce"])
+                all_metrics_df.loc["train_ppl"] = math.exp(all_metrics_df["train_ce"])
+                all_metrics_df.loc["val_ppl"] = math.exp(all_metrics_df["val_ce"])
             test_metrics_path = os.path.join(dir_experiment, "test_metrics_mean_sampling.csv")
             if os.path.exists(test_metrics_path):
                 test_metrics_exp = pd.read_csv(test_metrics_path, index_col=0, header=None)
-                all_metrics_df.append(test_metrics_exp.squeeze())
+                all_metrics_df = all_metrics_df.append(test_metrics_exp.squeeze())
             dir_name = os.path.split(dir_experiment)[-1]
             all_metrics_all_runs_df[dir_name] = all_metrics_df
         stats_all_runs_df["mean"] = all_metrics_all_runs_df.mean(axis=1)
         stats_all_runs_df["std"] = all_metrics_all_runs_df.std(axis=1)
+        stats_all_runs_df = stats_all_runs_df.dropna(axis=1)
+        if len(stats_all_runs_df.columns) > 1:
+            merge_metrics[os.path.basename(dir_conf)] = stats_all_runs_df["mean"].apply(lambda t: str(round(t, precision)) + '+/-') + stats_all_runs_df["std"].apply(lambda t: str(round(t, 3)))
+        else:
+            merge_metrics[os.path.basename(dir_conf)] = stats_all_runs_df["mean"].apply(lambda t: str(round(t, precision)))
         stats_all_runs_df.to_csv(os.path.join(dir_conf, "all_metrics.csv"))
-        all_metrics_all_runs_df.to_csv(os.path.join(dir_conf), "all_metrics_per_run.csv")
+        if len(dirs) > 1:
+            all_metrics_all_runs_df.to_csv(os.path.join(dir_conf, "all_metrics_per_run.csv"))
+    merge_metrics.to_csv(os.path.join(path, "merge_metrics.csv"))
 
 # def merge_one_experiment(args):
 #     dirs = [f.path for f in os.scandir(args.path) if f.is_dir()]
@@ -145,9 +136,9 @@ def merge_one_experiment(path="output/NLP/CLEVR/NEW_EXP__OLD"):
 
 
 if __name__ == '__main__':
-    merge_one_experiment()
-    # parser = get_parser()
-    # args = parser.parse_args()
+    parser = get_parser()
+    args = parser.parse_args()
+    merge_one_experiment(path=args.path, precision=args.precision)
     # if args.bottom_folder == 1:
     #     merge_one_experiment(args)
     # if args.top_folder == 1:
