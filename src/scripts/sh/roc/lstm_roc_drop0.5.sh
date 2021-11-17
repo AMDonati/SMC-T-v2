061913e983dda1101cb3aa4bf6ac342173749dc0
AMDonati <alice.martindonati@gmail.com>
2021-11-17 09:24:40 +0100

    sh

--- src/scripts/sh/roc/lstm_roc_drop0.5.sh
+++ src/scripts/sh/roc/lstm_roc_drop0.5.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=drop0.1-lstm
+#SBATCH --job-name=drop0.5-lstm
 #SBATCH --qos=qos_gpu-t3
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
@@ -19,14 +19,14 @@ export PYTHONPATH=src:${PYTHONPATH}
 
 DATASET="roc"
 DATA_PATH="data/ROC"
-OUTPUT_PATH="output/NLP/ROC/LSTM/drop0.1"
+OUTPUT_PATH="output/NLP/ROC/LSTM/drop0.5"
 D_MODEL=128
 RNN_UNITS=128
 BS=32
 PARTICLES=1
 EP=20
 
-srun python -u src/scripts/run.py -dataset $DATASET -data_path $DATA_PATH -algo "lstm" -d_model $D_MODEL -rnn_units $RNN_UNITS -bs $BS -ep $EP -output_path $OUTPUT_PATH -max_seq_len 20 -pdrop 0.1 -temp 0.7
+srun python -u src/scripts/run.py -dataset $DATASET -data_path $DATA_PATH -algo "lstm" -d_model $D_MODEL -rnn_units $RNN_UNITS -bs $BS -ep $EP -output_path $OUTPUT_PATH -max_seq_len 20 -pdrop 0.5 -temp 0.7
 
 
 
