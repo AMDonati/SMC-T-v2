217b6337ed4897456f38e966a9c8126eb4aae9d2
AMDonati <alice.martindonati@gmail.com>
2022-04-01 18:40:42 +0200

    sh

--- src/scripts/sh/2022/synthetic_model_1_FIXLAG.sh
+++ src/scripts/sh/2022/synthetic_model_1_FIXLAG.sh
@@ -17,11 +17,11 @@ conda activate smc-t
 
 export PYTHONPATH=src:${PYTHONPATH}
 
-#srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 4
-#srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 8
-#srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 12
-#srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 16
-srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 20
-srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 24
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/cv/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 4 -cv 1
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/cv/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 8 -cv 1
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/cv/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 12 -cv 1
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/cv/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 16 -cv 1
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/cv/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 20 -cv 1
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 10 -smc True -output_path "output/FIX_LAG/cv/exp_synthetic_model_1" -alpha 0.8 -mc_samples 1000 -fix_lag 24 -cv 1
 
 
