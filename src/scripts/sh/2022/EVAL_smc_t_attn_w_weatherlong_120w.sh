5337fdc6474374be81c2725b4792be58fe3666b5
AMDonati <alice.martindonati@gmail.com>
2022-04-11 20:31:36 +0200

    sh

--- src/scripts/sh/2022/EVAL_smc_t_attn_w_weatherlong_120w.sh
+++ src/scripts/sh/2022/EVAL_smc_t_attn_w_weatherlong_120w.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=EVAL120weather-attn-w-smct
-#SBATCH --qos=qos_gpu-t3
+#SBATCH --qos=qos_gpu-t4
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8g
 #SBATCH --output=slurm_out/attn-w-120w-%j.out
 #SBATCH --error=slurm_out/attn-w-120w-%j.err
-#SBATCH --time=20:00:00
+#SBATCH --time=100:00:00
 #SBATCH -A ktz@gpu
 
 export TMPDIR=$JOBSCRATCH
@@ -19,5 +19,5 @@ export PYTHONPATH=src:${PYTHONPATH}
 
 
 
-srun python -u src/scripts/run.py -dataset "weather_long" -dataset_model 1 -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 0 -particles 10 -smc True -output_path "output/WEATHER_LONG/attn_window" -alpha 0.8 -mc_samples 1000 -ess 0 -cv 0 -attn_w 120 -multistep 1 -past_len 120 -max_samples 10000 -save_path "output/WEATHER_LONG/attn_window/smc_t_l1_h1_d32_10p_attn-w120/1" -fix_lag 144
+srun python -u src/scripts/run.py -dataset "weather_long" -dataset_model 1 -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 12 -particles 10 -smc True -output_path "output/WEATHER_LONG/attn_window" -alpha 0.8 -mc_samples 1000 -ess 1 -cv 0 -attn_w 120 -multistep 1 -past_len 120 -max_samples 10000 -save_path "output/WEATHER_LONG/attn_window/smc_t_l1_h1_d32_10p_lagNone_ESS_attn-w120/1"
 
