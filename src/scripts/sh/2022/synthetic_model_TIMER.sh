ddaa3f5d2cbf4aaf99d360523a60201d31546b47
AMDonati <alice.martindonati@gmail.com>
2022-04-11 18:25:35 +0200

    sh

---
+++ src/scripts/sh/2022/synthetic_model_TIMER.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH --job-name=TIMER
+#SBATCH --qos=qos_gpu-t3
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=8g
+#SBATCH --output=slurm_out/TIMER-synthetic-%j.out
+#SBATCH --error=slurm_out/TIMER-synthetic-%j.err
+#SBATCH --time=20:00:00
+#SBATCH -A ktz@gpu
+
+export TMPDIR=$JOBSCRATCH
+module purge
+module load  pytorch-gpu/py3/1.7.1
+conda activate smc-t
+
+export PYTHONPATH=src:${PYTHONPATH}
+
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/TIMER_synthetic" -alpha 0.8 -mc_samples 5 -cv 0 -ess 0
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/TIMER_synthetic" -alpha 0.8 -mc_samples 5 -cv 0 -ess 1
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/TIMER_synthetic" -alpha 0.8 -mc_samples 5 -fix_lag 4 -cv 0 -ess 0
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/TIMER_synthetic" -alpha 0.8 -mc_samples 5 -fix_lag 4 -cv 0 -ess 1
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/TIMER_synthetic" -alpha 0.8 -mc_samples 5 -fix_lag 20 -cv 0 -ess 0
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/TIMER_synthetic" -alpha 0.8 -mc_samples 5 -fix_lag 20 -cv 0 -ess 1
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/TIMER_synthetic" -alpha 0.8 -mc_samples 5 -cv 0 -ess 0 -attn_w 20
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/TIMER_synthetic" -alpha 0.8 -mc_samples 5 -cv 0 -ess 1 -attn_w 20
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/TIMER_synthetic" -alpha 0.8 -mc_samples 5 -fix_lag 4 -cv 0 -ess 0 -attn_w 20
+srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/TIMER_synthetic" -alpha 0.8 -mc_samples 5 -fix_lag 4 -cv 0 -ess 1 -attn_w 20
+
+
+#srun python -u src/scripts/run.py -dataset "synthetic" -dataset_model 1 -data_path "data/synthetic_model_1" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 50 -particles 60 -smc True -output_path "output/ESS/" -alpha 0.8 -mc_samples 1000 -ess True -cv 1
+
+
