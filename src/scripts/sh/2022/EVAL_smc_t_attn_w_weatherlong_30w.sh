fcb8feb5b48cf4266cb054c4a7064ac3ef2465c2
AMDonati <alice.martindonati@gmail.com>
2022-04-11 17:04:28 +0200

    sh

--- src/scripts/sh/2022/EVAL_smc_t_attn_w_weatherlong_30w.sh
+++ src/scripts/sh/2022/EVAL_smc_t_attn_w_weatherlong_30w.sh
@@ -18,4 +18,4 @@ conda activate smc-t
 export PYTHONPATH=src:${PYTHONPATH}
 
 
-srun python -u src/scripts/run.py -dataset "weather_long" -dataset_model 1 -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 0 -particles 10 -smc True -output_path "output/WEATHER_LONG/attn_window" -alpha 0.8 -mc_samples 1000 -ess 0 -cv 0 -attn_w 30 -multistep 1 -past_len 120 -max_samples 10000 -save_path "output/WEATHER_LONG/attn_window/w30/1"
+srun python -u src/scripts/run.py -dataset "weather_long" -dataset_model 1 -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 0 -particles 10 -smc True -output_path "output/WEATHER_LONG/attn_window" -alpha 0.8 -mc_samples 1000 -ess 0 -cv 0 -attn_w 30 -multistep 1 -past_len 120 -max_samples 10000 -save_path "output/WEATHER_LONG/attn_window/w30/1" -fix_lag 144
