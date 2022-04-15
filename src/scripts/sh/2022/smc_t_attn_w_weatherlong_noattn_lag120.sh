a57994e8bd1374aa6d2a77653909770b0c026274
AMDonati <alice.martindonati@gmail.com>
2022-04-15 08:41:49 +0200

    sh

--- src/scripts/sh/2022/smc_t_attn_w_weatherlong_noattn_lag120.sh
+++ src/scripts/sh/2022/smc_t_attn_w_weatherlong_noattn_lag120.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=NOattn120lagweather-smct
-#SBATCH --qos=qos_gpu-t4
+#SBATCH --job-name=120lagweather-smct
+#SBATCH --qos=qos_gpu-t3
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=8g
 #SBATCH --output=slurm_out/fixlag144-%j.out
 #SBATCH --error=slurm_out/fixlag144-120w-%j.err
-#SBATCH --time=100:00:00
+#SBATCH --time=20:00:00
 #SBATCH -A ktz@gpu
 
 export TMPDIR=$JOBSCRATCH
@@ -18,6 +18,6 @@ conda activate smc-t
 export PYTHONPATH=src:${PYTHONPATH}
 
 
-srun python -u src/scripts/run.py -dataset "weather_long" -dataset_model 1 -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 20 -particles 10 -smc True -output_path "output/WEATHER_LONG/fix_lag" -alpha 0.8 -mc_samples 1000 -ess 1 -cv 0 -multistep 1 -past_len 120 -max_samples 10000 -fix_lag 120
-srun python -u src/scripts/run.py -dataset "weather_long" -dataset_model 1 -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 20 -particles 10 -smc True -output_path "output/WEATHER_LONG/fix_lag" -alpha 0.8 -mc_samples 1000 -ess 0 -cv 0 -multistep 1 -past_len 120 -max_samples 10000 -fix_lag 120
+srun python -u src/scripts/run.py -dataset "weather_long" -dataset_model 1 -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 20 -particles 10 -smc True -output_path "output/WEATHER_LONG/fix_lag" -alpha 0.8 -mc_samples 1000 -ess 1 -cv 0 -multistep 1 -past_len 120 -max_samples 10000 -fix_lag 120 -save_path "output/WEATHER_LONG/fix_lag/smc_t_l1_h1_d32_10p_lag120_ESS/1"
+#srun python -u src/scripts/run.py -dataset "weather_long" -dataset_model 1 -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 20 -particles 10 -smc True -output_path "output/WEATHER_LONG/fix_lag" -alpha 0.8 -mc_samples 1000 -ess 0 -cv 0 -multistep 1 -past_len 120 -max_samples 10000 -fix_lag 120
 
