bd793f859afe68ff2adaa956e182a4f0a0229e4a
AMDonati <alice.martindonati@gmail.com>
2022-04-15 07:42:25 +0200

    sh

--- src/scripts/sh/2022/WEATHERLONG_TIMER_2.sh
+++ src/scripts/sh/2022/WEATHERLONG_TIMER_2.sh
@@ -18,28 +18,28 @@ conda activate smc-t
 export PYTHONPATH=src:${PYTHONPATH}
 
 # attentionwindow = 30
-srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -attn_w 30 -multistep 0 -past_len 120 -max_samples 10000
-srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess 0 -cv 0 -attn_w 30 -multistep 0 -past_len 120 -max_samples 10000
+srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -attn_w 30 -multistep 0 -past_len 120 -max_samples 10000
+srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess 0 -cv 0 -attn_w 30 -multistep 0 -past_len 120 -max_samples 10000
 # attentionwindow=30, fix_lag=30.
 srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -attn_w 30 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 30
-srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess 0 -cv 0 -attn_w 30 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 30
+srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess 0 -cv 0 -attn_w 30 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 30
 # fixlag = 30
 srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 30
-srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess 0 -cv 0 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 30
+srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess 0 -cv 0 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 30
 # no lag, no attention window
-srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -multistep 0 -past_len 120 -max_samples 10000
-srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess 0 -cv 0 -multistep 0 -past_len 120 -max_samples 10000
+srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -multistep 0 -past_len 120 -max_samples 10000
+srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess 0 -cv 0 -multistep 0 -past_len 120 -max_samples 10000
 # Baselines
 python src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "baseline_t" -d_model 32 -dff 32 -bs 64 -ep 1 -output_path -output_path "output/WEATHER_LONG/TIMER_2" -p_drop 0.1 -mc_samples 1000 -max_samples 10000 -past_len 120 -multistep 1 -pe 500
 # attentionwindow = 120
-srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -attn_w 120 -multistep 0 -past_len 120 -max_samples 10000
-srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess 0 -cv 0 -attn_w 120 -multistep 0 -past_len 120 -max_samples 10000
+srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -attn_w 120 -multistep 0 -past_len 120 -max_samples 10000
+srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess 0 -cv 0 -attn_w 120 -multistep 0 -past_len 120 -max_samples 10000
 # attentionwindow=60, fix_lag=60, ESS.
-srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -attn_w 60 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 60
+srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 64 -ep 1 -particles 10 -smc True -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -attn_w 60 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 60
 # fixlag 60, ESS
-srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 60
+srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 60
 # fixlag 120, ESS
-srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 120
+srun python -u src/scripts/run.py -dataset "weather_long" -data_path "data/weather_long" -algo "smc_t" -d_model 32 -dff 32 -bs 32 -ep 1 -particles 10 -smc True -output_path "output/WEATHER_LONG/TIMER_2" -alpha 0.8 -mc_samples 5 -ess True -cv 0 -multistep 0 -past_len 120 -max_samples 10000 -fix_lag 120
 
 
 
